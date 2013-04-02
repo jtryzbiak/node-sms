2541da982f5845ba40271f8dc206d7a0ee1d4510
Jon Tryzbiak <jon.tryzbiak@gmail.com>
2013-04-02 19:58:35 -0400

    updated conversations json

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,8 @@
       res.render('index', { phone_number: '555-555-5555' });
     });
 
-    app.get('/conversations', function(req, res){
-
-       var recentConversations = {
+    app.get('/conversations', function(req, res) {
+       var recentConversations = {conversations: [
           {
             id: 1,
             with: '111-222-3333',
@@ -14,8 +13,8 @@
             timeStamp: new Date(2013, 03, 29),
             body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sem est, placerat eget mattis eu, accumsan eu nisl. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos.',
           }
-       };
-       res.json(conversations);
+        ]};
+       res.json(recentConversations);
     });
 
     app.post('/message/send', function(req, res) {
